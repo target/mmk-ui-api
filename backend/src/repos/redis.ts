c6d86943cd9aefd79b9ba3b78a4a37e43f54ca58
Caleb Walch <caleb.walch@target.com>
2021-12-16 13:52:40 -0600

    build(ci/cd): testing GH actions

--- backend/src/repos/redis.ts
+++ backend/src/repos/redis.ts
@@ -2,17 +2,21 @@ import redis from 'ioredis'
 import { config } from 'node-config-ts'
 
 function createClient(): redis.Redis {
-  if (config.redis?.useSentinel && config.redis.nodes) {
+  if (
+    config.redis?.useSentinel &&
+    config.redis.nodes &&
+    Array.isArray(config.redis.nodes)
+  ) {
     const clients = config.redis.nodes.map((item: string) => ({
       host: item.trim(),
-      port: config.redis.sentinelPort,
+      port: config.redis.sentinelPort
     }))
     return new redis({
       updateSentinels: false,
       sentinels: clients,
       name: config.redis.master,
       password: config.redis.sentinelPassword,
-      sentinelPassword: config.redis.sentinelPassword,
+      sentinelPassword: config.redis.sentinelPassword
     })
   }
   return new redis(config.redis.uri)
