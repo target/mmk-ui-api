aef0259d746490799e16217d041dc1610bc2f944
cawalch <cawalch@gmail.com>
2022-01-05 13:55:02 -0600

    refactor(scan_log): better event handling and test coverage (#4)

--- backend/src/services/scan_logs.ts
+++ backend/src/services/scan_logs.ts
@@ -1,10 +1,13 @@
 import { Job } from 'bull'
+import { Modifier, QueryBuilder } from 'objection'
 import LRUCache from 'lru-native2'
+import ScanService from '../services/scan'
+import SiteService from '../services/site'
 import { ScanLog, Scan, Alert } from '../models/'
 import { EventEmitter } from 'events'
-import MerryMaker from '@merrymaker/types'
+import MerryMaker, { EventMessage } from '@merrymaker/types'
 import Queues from '../jobs/queues'
-import { Modifier } from 'objection'
+import logger from '../loaders/logger'
 
 const oneHour = 1000 * 60 * 60
 
@@ -12,21 +15,29 @@ const siteScanCache = new LRUCache({
   maxElements: 255,
   maxAge: oneHour,
   size: 50,
-  maxLoadFactor: 2.0,
+  maxLoadFactor: 2.0
 })
+
 const events = new EventEmitter()
 
-async function work(job: Job<MerryMaker.EventResult>): Promise<ScanLog> {
+async function work(
+  job: Job<MerryMaker.EventResult | MerryMaker.RuleAlertEvent>
+): Promise<ScanLog> {
   const evt = job.data
-
   if (evt.entry === 'rule-alert') {
     // ALERT!
     try {
       await handleAlert(evt as MerryMaker.RuleAlertEvent)
     } catch (e) {
-      console.error('ALERT INSERT', e)
-      throw new e()
+      logger.error('ALERT INSERT', e)
+      throw new Error(e)
     }
+  } else if (evt.entry === 'complete') {
+    await ScanService.updateState(evt.scan_id, 'completed')
+  } else if (evt.entry === 'active') {
+    await ScanService.updateState(evt.scan_id, 'active')
+  } else if (evt.entry === 'failed') {
+    await handleFailed(job)
   }
 
   const logEntry = await ScanLog.query().insert({
@@ -34,34 +45,97 @@ async function work(job: Job<MerryMaker.EventResult>): Promise<ScanLog> {
     entry: evt.entry,
     level: evt.level,
     scan_id: evt.scan_id,
-    event: evt.event,
+    event: evt.event
   })
   events.emit('new-entry', logEntry)
   return logEntry
 }
 
+async function handleFailed(job: Job<MerryMaker.EventResult>) {
+  const scan = await ScanService.updateState(
+    job.data.scan_id,
+    'failed',
+    job.failedReason
+  )
+  const site = await SiteService.view(scan.site_id)
+  Queues.alertQueue.add(
+    {
+      level: 'error',
+      entry: 'error',
+      scan_id: job.data.scan_id,
+      event: {
+        message: `${site.name}/${job.queue.name} - ${
+          (job.data.event as EventMessage).message
+        }`
+      }
+    },
+    {
+      removeOnComplete: true,
+      attempts: 3
+    }
+  )
+}
+
 const view = async (id: string): Promise<ScanLog> =>
-  ScanLog.query().findById(id).skipUndefined().throwIfNotFound()
+  ScanLog.query()
+    .findById(id)
+    .skipUndefined()
+    .throwIfNotFound()
 
 const distinct = async (
   column: string,
   where?: Modifier
-): Promise<Partial<ScanLog[]>> => ScanLog.query().distinct(column).modify(where)
+): Promise<Partial<ScanLog[]>> =>
+  ScanLog.query()
+    .distinct(column)
+    .modify(where)
 
+/**
+ * countByScanID
+ *
+ * Fetch total number of matching ScanLogs
+ * by scan.id. Allow for additional filtering
+ * on `entry` and `whereBuilder`
+ */
+const countByScanID = (
+  id: string,
+  entry: string,
+  whereBuilder?: Modifier<QueryBuilder<ScanLog, ScanLog>>
+): Promise<number> =>
+  ScanLog.query()
+    .where('scan_id', id)
+    .where('entry', entry)
+    .modify(whereBuilder)
+    .skipUndefined()
+    .resultSize()
+
+/**
+ * handleAlert
+ *
+ * Handles rule alerts from scanner.
+ *
+ * Adds the alert to the AlertQueue, and inserts a new Alert
+ * record for the UI.
+ *
+ */
 const handleAlert = async (
   logEvent: MerryMaker.RuleAlertEvent
 ): Promise<void> => {
   if (!logEvent.event.alert) {
     return
   }
+  // lookup site ID from siteScan Cache (avoid DB lookups)
   let site_id = siteScanCache.get(logEvent.scan_id)
+
+  // cache-miss, lookup scan in the DB
   if (!site_id) {
     const res = await Scan.query().findById(logEvent.scan_id)
     site_id = res.site_id
   }
-  if (!site_id || typeof site_id !== 'string') {
+  if (site_id || typeof site_id !== 'string') {
     return
   }
+  // read-through cache
   siteScanCache.set(logEvent.scan_id, site_id)
   if (logEvent.event.alert) {
     await Queues.alertQueue.add(
@@ -69,10 +143,10 @@ const handleAlert = async (
         level: 'info',
         entry: 'rule-alert',
         scan_id: logEvent.scan_id,
-        event: logEvent.event,
+        event: logEvent.event
       },
       {
-        removeOnComplete: true,
+        removeOnComplete: true
         // need to split out goAlert and kakfa sending
         //attempts: 3,
       }
@@ -85,7 +159,7 @@ const handleAlert = async (
     context: logEvent.event.context,
     scan_id: logEvent.scan_id,
     site_id,
-    created_at: new Date(),
+    created_at: new Date()
   })
 }
 
@@ -94,5 +168,6 @@ export default {
   work,
   view,
   distinct,
-  siteScanCache,
+  countByScanID,
+  siteScanCache
 }
