a69ff0279791654a243b0bc43fc1fa3a855fc68c
krautface <43364319+krautface@users.noreply.github.com>
2022-02-10 23:49:07 -0600

    add htmlsnapshot

---
+++ scanner/src/rules/html-snapshot.ts
@@ -0,0 +1,80 @@
+import path from 'path'
+
+import YaraSync from '../lib/yara-sync'
+import * as MerryMaker from '@cfc/merry-maker-types'
+import { Rule } from './base'
+
+import LRUCache from 'lru-native2'
+import crypto from 'crypto'
+
+
+const twoWeeks = 1000 * 60 * 60 * 24 * 7 * 2
+const yara = new YaraSync()
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+;(async () => {
+  // init yara skimmer rules
+  await yara.initAsync({
+    rules: [{ filename: path.resolve(__dirname, 'skimmer.yara') }]
+  })
+})()
+
+const snapshotHashCache = new LRUCache({
+  maxElements: 10000,
+  maxAge: twoWeeks,
+  size: 50,
+  maxLoadFactor: 2.0
+})
+
+/**
+ * HTMLSnapShot rule
+ *
+ * Checks for malicious content in page content
+ *
+ */
+export class HtmlSnapshotRule extends Rule {
+  alertResults: MerryMaker.RuleAlert[]
+  async process(
+    payload: MerryMaker.HtmlSnapshot
+  ): Promise<MerryMaker.RuleAlert[]> {
+    this.alertResults = []
+    const res: MerryMaker.RuleAlert = {
+      name: this.options.name,
+      alert: false,
+      message: 'no alert',
+      level: this.options.level,
+      context: { url: payload.url }
+    }
+
+    if (payload.html) {
+      const hash = crypto.createHash('sha256').update(payload.html).digest('base64');
+      if (snapshotHashCache.get(hash)) {
+        res.message = `seen HTMLSnapshot (cache) ${hash}`
+        return this.resolveEvent(res)
+      }
+
+      snapshotHashCache.set(hash, 1)
+
+      const result = await yara.scanAsync({
+        buffer: Buffer.from(payload.html, 'utf-8')
+      })
+      if (result.rules.length) {
+        this.alertResults = result.rules.map(r => ({
+          ...res,
+          alert: true,
+          message: `${r.id} hit`
+        }))
+      }
+    }
+
+    return this.resolveEvent(res)
+  }
+}
+
+export default new HtmlSnapshotRule({
+  name: 'HTMLSnapshotRule',
+  level: 'prod',
+  alert: false,
+  context: {},
+  description: 'detects malicious page content'
+})
--- scanner/src/rules/index.ts
+++ scanner/src/rules/index.ts
@@ -5,6 +5,7 @@ import iocPayloadRule from './ioc.payload'
 import yaraRule from './yara'
 import webSocketRule from './websocket'
 import googleAnalyticsRule from './google-analytics'
+import htmlSnapshot from './html-snapshot'
 
 const scanHandler = new ScanEventHandler()
 
@@ -15,5 +16,6 @@ scanHandler.use('request', iocPayloadRule)
 scanHandler.use('request', googleAnalyticsRule)
 scanHandler.use('script-response', yaraRule)
 scanHandler.use('function-call', webSocketRule)
+scanHandler.use('html-snapshot', htmlSnapshot)
 
 export { scanHandler }
