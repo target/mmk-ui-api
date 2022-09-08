99dd808b3b8dc41a59b508fbfee1253219fc28de
cawalch <cawalch@gmail.com>
2022-09-08 08:29:50 -0500

    rules(websocket): scope test scans

--- scanner/src/rules/websocket.ts
+++ scanner/src/rules/websocket.ts
@@ -7,39 +7,41 @@ import { IResult } from 'tldts-core'
 
 const oneHour = 1000 * 60 * 60
 
-const domainAllowListCache = new LRUCache({
+export const domainAllowListCache = new LRUCache<number>({
   maxElements: 1000,
   maxAge: oneHour,
   size: 50,
-  maxLoadFactor: 2.0,
+  maxLoadFactor: 2.0
 })
 
-const seenDomainCache = new LRUCache({
+export const seenDomainCache = new LRUCache<number>({
   maxElements: 10000,
   maxAge: oneHour,
   size: 1000,
-  maxLoadFactor: 2.0,
+  maxLoadFactor: 2.0
 })
 
-const iocDomainCache = new LRUCache({
+export const iocDomainCache = new LRUCache<number>({
   maxElements: 1000,
   maxAge: oneHour,
   size: 1000,
-  maxLoadFactor: 2.0,
+  maxLoadFactor: 2.0
 })
 
 export class WebsocketRule extends Rule {
   alertResults: MerryMaker.RuleAlert[]
+  payloadURL: IResult
   async process(
     scanEvent: MerryMaker.ScanEvent
   ): Promise<MerryMaker.RuleAlert[]> {
+    this.event = scanEvent
     const payload = scanEvent.payload as MerryMaker.WebFunctionCallEvent
     this.alertResults = []
     const res: MerryMaker.RuleAlert = {
       name: this.options.name,
       alert: false,
       level: this.options.level,
-      context: { url: payload.args },
+      context: { url: payload.args }
     }
 
     if (payload.func.toLocaleLowerCase() !== 'websocket') {
@@ -47,66 +49,65 @@ export class WebsocketRule extends Rule {
       return this.resolveEvent(res)
     }
 
-    let url = payload.args.toString()
+    const url = payload.args.toString()
     // Handle invalid URLs
     let payloadURL: IResult
     try {
-      payloadURL = parse(url)
+      this.payloadURL = parse(url)
     } catch (e) {
       res.message = `failed to parse URL ${e.message}`
       return this.resolveEvent(res)
     }
 
-    if (payloadURL.domain.length === 0) {
+    if (
+      typeof this.payloadURL.domain !== 'string' ||
+      this.payloadURL.domain.length === 0
+    ) {
       res.message = `missing / empty domain for payload (${url})`
       return this.resolveEvent(res)
     }
 
     // Check allow_list cache
-    if (domainAllowListCache.get(payloadURL.domain)) {
+    const allowedDomain = await this.isAllowed({
+      value: this.payloadURL.domain,
+      key: 'fqdn',
+      cache: domainAllowListCache
+    })
+
+    if (allowedDomain) {
       res.message = `allow-listed (cache) ${payloadURL.domain}`
       return this.resolveEvent(res)
     }
 
-    const allowList = await this.fetchRemoteAllowList(payloadURL.domain, 'fqdn')
-    // Checkout backend transport
-    if (allowList.total > 0) {
-      res.message = `allow-listed (DB) ${payloadURL.domain}`
-      domainAllowListCache.set(payloadURL.domain, 1)
-      return this.resolveEvent(res)
-    }
-
     // Check if it is an IOC first
-    if (iocDomainCache.get(payloadURL.hostname)) {
+    if (iocDomainCache.get(this.payloadURL.hostname)) {
       res.alert = true
-      res.message = `known IOC (cache) ${payloadURL.hostname}`
+      res.message = `known IOC (cache) ${this.payloadURL.hostname}`
       return this.resolveEvent(res)
     }
 
-    const iocs = await this.fetchRemoteIOC(payloadURL.hostname)
+    const iocs = await this.fetchRemoteIOC(this.payloadURL.hostname)
     if (iocs.total > 0) {
       res.alert = true
-      res.message = `known IOC (DB) ${payloadURL.hostname}`
-      iocDomainCache.set(payloadURL.hostname, 1)
+      res.message = `known IOC (DB) ${this.payloadURL.hostname}`
+      iocDomainCache.set(this.scopeValue(this.payloadURL.hostname), 1)
       return this.resolveEvent(res)
     }
 
+    const seenDomain = await this.wasSeen({
+      value: this.payloadURL.hostname,
+      key: 'domain',
+      cache: seenDomainCache
+    })
+
     // Check full hostname in seen domain cache
-    if (seenDomainCache.get(payloadURL.hostname) === 1) {
+    if (seenDomain.store !== 'none') {
       res.message = `seen hostname (cache) ${payloadURL.hostname}`
       return this.resolveEvent(res)
     }
 
-    // Check remote cache
-    const seenData = await this.bumpRemoteCache(payloadURL.hostname, 'domain')
-
-    // cache the result
-    seenDomainCache.set(payloadURL.hostname, 1)
-    // Alert if domain was not found in any store
-    if (seenData.store === 'none') {
-      res.alert = true
-      res.message = `${payloadURL.hostname} unknown`
-    }
+    res.alert = true
+    res.message = `${this.payloadURL.hostname} unknown`
 
     return this.resolveEvent(res)
   }
@@ -117,5 +118,5 @@ export default new WebsocketRule({
   level: 'prod',
   alert: false,
   context: {},
-  description: 'detects unknown/IOC domains in websocket connections',
+  description: 'detects unknown/IOC domains in websocket connections'
 })
