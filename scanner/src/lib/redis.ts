8d9695936081e82feab77e1b1b280ceccdf7877c
cawalch <cawalch@gmail.com>
2022-09-08 08:17:38 -0500

    august dependencies updates (#65)

--- scanner/src/lib/redis.ts
+++ scanner/src/lib/redis.ts
@@ -1,18 +1,24 @@
-import redis from 'ioredis'
+import redis, { RedisOptions } from 'ioredis'
 
 import { config } from 'node-config-ts'
+
+const defaultOpts: RedisOptions = {
+  maxRetriesPerRequest: null,
+  enableReadyCheck: false
+}
 
 function createClient(): redis.Redis {
   if (config.redis.useSentinel) {
     const clients = config.redis.nodes.map((item: string) => ({
       host: item.trim(),
-      port: config.redis.sentinelPort,
+      port: config.redis.sentinelPort
     }))
     return new redis({
       updateSentinels: false,
       sentinels: clients,
       name: config.redis.master,
       password: config.redis.sentinelPassword,
+      ...defaultOpts
     })
   }
   return new redis(config.redis.uri)
